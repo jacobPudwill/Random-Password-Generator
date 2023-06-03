3c5caee9754cc6e85f6b9576ad355331c973e142
Jacob Pudwill <jacobpudwill@gmail.com>
2023-06-03 16:29:41 -0500

    Added get Password Characters function to JavaScript file that when called prompts the user to answer whether they would like lowcase, uppercase, numerical, and/or special characters to be included in the password. At least one type of character must be chosen.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Prompts the user for what the length of the password should be, accepted input being a number in the range 8-128
+// Prompts the user for what the length of the password should be and returns it
+// accepted input being a number in the range 8-128
 function getPasswordLength(){
     var passwordLength = parseInt(prompt("Length of password:"));
     
@@ -12,10 +13,45 @@ function getPasswordLength(){
     return passwordLength;
 }
 
+// Prompts the user for the character types that should be included in the password and returns a string of selected character types
+// accepted input being at least one type
+function getPasswordCharacters() {
+    var charTypes = [false, false, false, false];
+    var passwordCharacters = "";
+
+    while (!charTypes[0] && !charTypes[1] && !charTypes[2] && !charTypes[3]){
+        charTypes[0] = confirm("Should the password include lowercase characters?");
+        charTypes[1] = confirm("Should the password include uppercase characters?");
+        charTypes[2] = confirm("Should the password include numeric characters?");
+        charTypes[3] = confirm("Should the password include special characters?");
+
+        if (charTypes[0]){
+            passwordCharacters += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if (charTypes[1]){
+            passwordCharacters += "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+        }
+        if (charTypes[2]){
+            passwordCharacters += "0123456789";
+        }
+        if (charTypes[3]){
+            passwordCharacters += " !\"#$%&'()*+,-./:;<=>?@[\\^_`{|}~";
+        }
+
+        if (!charTypes[0] && !charTypes[1] && !charTypes[2] && !charTypes[3]){
+            confirm("Please select at least one type of characters for the password.");
+        }
+    }
+
+    return passwordCharacters;
+}
+
 // Generates a random password that includes special characters
 function generatePassword(){
-    var passwordLength = getPasswordLength();    
-
+    var password = "";
+    var passwordLength = getPasswordLength();
+    var passwordCharacters = getPasswordCharacters();
+    
 }
 
 // Write password to the #password input
